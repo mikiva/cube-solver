08080d5a7398a6b05b9a578c7bda1d847db4b025
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2023-08-20 00:50:03 +0200

    now working correctly

--- UImain.py
+++ UImain.py
@@ -1,10 +1,9 @@
-import random
-
 from ursina import *
-from UIhelpers import after
 from helper import moves, annotation_to_move
 from cube import Cube as RubiksCube
 from functools import partial
+import time
+
 
 app = Ursina()
 
@@ -43,8 +42,8 @@ rotation_helper: Entity = None
 collider = None
 shuffle_button = None
 
+
 def collider_input(key):
-    global animations
     if mouse.hovered_entity == collider:
         f = [i for i in cube_faces if cube_faces[i] == mouse.normal][0]
         print(f)
@@ -53,95 +52,98 @@ def collider_input(key):
         if key == "right mouse down":
             twist(f, -1)
 
-    collider.ignore_input = True
+    # collider.ignore_input = True
 
-    @after(.25 * 1)
-    def _():
-        collider.ignore_input = False
-        # check_for_win()
+    # @after(.25 * 1)
+    # def _():
+    #    collider.ignore_input = False
+    #    # check_for_win()
+
 
 BASE_SPEED = 0.1
 TURN_DEGREES = 90
-def rotate_side(normal, direction=1, speed=1):
-    print("rotate_side", normal, direction)
+
+ANIMATION_FINISHED = True
+
+
+def _animated():
+    ...
+
+
+def face_rotation_animation_toggle():
+    global ANIMATION_FINISHED
+    ANIMATION_FINISHED = not ANIMATION_FINISHED
+
+
+CURVE = curve.linear
+INTERRUPT = "finish"
+
+
+def rotate_side(normal, direction=1, speed=1.0):
     animated = "UNKNOWN"
-    if normal == Vec3(1, 0, 0): #Right
-        def animated():
-
+    if normal == Vec3(1, 0, 0):  # Right
+        def animated(_speed=speed):
+            reset_rotation_helper()
             [setattr(e, "world_parent", rotation_helper) for e in cubes if e.x > 0]
-            return rotation_helper.animate("rotation_x", TURN_DEGREES * direction, duration=BASE_SPEED * speed,
-                                           curve=curve.linear,
-                                           interrupt="kill")
-    elif normal == Vec3(-1, 0, 0): #Left
-        def animated():
+            return rotation_helper.animate("rotation_x", TURN_DEGREES * direction, duration=BASE_SPEED * _speed,
+                                           curve=CURVE,
+                                           interrupt=INTERRUPT)
+    elif normal == Vec3(-1, 0, 0):  # Left
+        def animated(_speed=speed):
+            reset_rotation_helper()
             [setattr(e, "world_parent", rotation_helper) for e in cubes if e.x < 0]
-            return rotation_helper.animate("rotation_x", -TURN_DEGREES * direction, duration=BASE_SPEED * speed,
-                                           curve=curve.linear,
-                                           interrupt="kill")
+            return rotation_helper.animate("rotation_x", -TURN_DEGREES * direction, duration=BASE_SPEED * _speed,
+                                           curve=CURVE,
+                                           interrupt=INTERRUPT)
 
-    elif normal == Vec3(0, 1, 0): #Up
-        def animated():
+    elif normal == Vec3(0, 1, 0):  # Up
+        def animated(_speed=speed):
+            reset_rotation_helper()
             [setattr(e, "world_parent", rotation_helper) for e in cubes if e.y > 0]
-            return rotation_helper.animate("rotation_y", TURN_DEGREES * direction, duration=BASE_SPEED * speed,
-                                           curve=curve.linear,
-                                           interrupt="kill")
-    elif normal == Vec3(0, -1, 0): #Down
-        def animated():
+            return rotation_helper.animate("rotation_y", TURN_DEGREES * direction, duration=BASE_SPEED * _speed,
+                                           curve=CURVE,
+                                           interrupt=INTERRUPT)
+    elif normal == Vec3(0, -1, 0):  # Down
+        def animated(_speed=speed):
+            reset_rotation_helper()
 
             [setattr(e, "world_parent", rotation_helper) for e in cubes if e.y < 0]
-            return rotation_helper.animate("rotation_y", -TURN_DEGREES * direction, duration=BASE_SPEED * speed,
-                                           curve=curve.linear,
-                                           interrupt="kill")
+            return rotation_helper.animate("rotation_y", -TURN_DEGREES * direction, duration=BASE_SPEED * _speed,
+                                           curve=CURVE,
+                                           interrupt=INTERRUPT)
 
-    elif normal == Vec3(0, 0, 1): #Back
-        def animated():
+    elif normal == Vec3(0, 0, 1):  # Back
+        def animated(_speed=speed):
+            reset_rotation_helper()
             [setattr(e, "world_parent", rotation_helper) for e in cubes if e.z > 0]
-            rotation_helper.animate("rotation_z", -TURN_DEGREES * direction, duration=BASE_SPEED * speed,
-                                    curve=curve.linear,
-                                    interrupt="kill")
-    elif normal == Vec3(0, 0, -1): #Front
-        def animated():
+            return rotation_helper.animate("rotation_z", -TURN_DEGREES * direction, duration=BASE_SPEED * _speed,
+                                           curve=CURVE,
+                                           interrupt=INTERRUPT)
+    elif normal == Vec3(0, 0, -1):  # Front
+        def animated(_speed=speed):
+            reset_rotation_helper()
             [setattr(e, "world_parent", rotation_helper) for e in cubes if e.z < 0]
-            return rotation_helper.animate("rotation_z", TURN_DEGREES * direction, duration=BASE_SPEED * speed,
-                                           curve=curve.linear,
-                                           interrupt="kill")
+            return rotation_helper.animate("rotation_z", TURN_DEGREES * direction, duration=BASE_SPEED * _speed,
+                                           curve=CURVE,
+                                           interrupt=INTERRUPT)
 
-    # if animated:
-    #    print("ANIMATED", animated.finished)
-    # invoke(reset_rotation_helper, delay=.22 * speed)
+
     return animated
 
+
 def twist(face, direction):
-    print("twist", face, direction)
     animation = rotate_side(cube_faces[face[0]], direction=direction, speed=1)
-    print("ani", animation)
     if animation:
         animations.append(animation)
         f = f"{face}\'" if direction < 0 else face
-        print("121", f)
         rubiks.twist_cube([moves[f]])
         rubiks.show()
-
-
-
-
-#
-# if speed:
-#    collider.ignore_input = True
-#
-#    @after(.25 * speed)
-#    def _():
-#        collider.ignore_input = False
-#        # check_for_win()
 
 
 def reset_rotation_helper():
     global cubes, rotation_helper
     [setattr(e, "world_parent", scene) for e in cubes]
     rotation_helper.rotation = (0, 0, 0)
-
-
-# cube = Entity(model='cube', color=hsv(300, 1, 1), scale=2, collider='box')
 
 
 def randomize():
@@ -151,41 +153,41 @@ def randomize():
         rotate_side(normal=random.choice(faces), direction=random.choice((-1, 1)), speed=0)
 
 
+debug_text = Text(text="debug", origin=(0, 18), color=color.white)
 
 
-def shuffle():
-    #print(moves.keys())
+def shuffle(speed=1):
+    # print(moves.keys())
     all_moves = list(moves.keys())
 
     for i in range(20):
         a = random.choice(all_moves)
         direction = -1 if "\'" in a else 1
-        print("shuffle move", a, direction)
-        animation = rotate_side(cube_faces[a[0]], direction=direction, speed=1)
+        animation = rotate_side(cube_faces[a[0]], direction=direction, speed=speed)
         if animation is not None:
             animations.append(animation)
             rubiks.twist_cube([moves[a]])
 
         # time.sleep(0.4)
-
+    debug_text.text = rubiks.get_solution()
     rubiks.show()
 
-debug_text = Text(text=len(cubes), x=.3, y=-.2)
 
 
-import time
 last_animation = 0
+
+
 def update():
     global current_animation, shuffle_button, animations, last_animation
-    if len(animations) > 0:
-        if current_animation == "INIT" or time.time() > last_animation + 0.15:
 
+    if len(animations) > 0:
+        if current_animation == "INIT" or current_animation.finished:  # time.time() > last_animation + BASE_SPEED + .01:
             next_animation = animations.pop(0)
             if next_animation:
-                invoke(reset_rotation_helper)
                 animation_result = next_animation()
                 if animation_result is not None:
                     current_animation = animation_result
+
             last_animation = time.time()
         shuffle_button.enabled = False
 
@@ -194,13 +196,12 @@ def update():
         shuffle_button.enabled = True
 
 
-
 def input(key):
     ...
     # print(key)
 
 
-def solve():
+def solve(speed=1):
     global animations
     algorithm = rubiks.get_solution()
     print(algorithm)
@@ -209,25 +210,24 @@ def solve():
         move = annotation_to_move(a)
         rubiks.twist_cube(move)
         if "2" in a:
-            rot1 = rotate_side(cube_faces[a[0]])
-            rot2 = rotate_side(cube_faces[a[0]])
+            rot1 = rotate_side(cube_faces[a[0]], speed=speed)
+            rot2 = rotate_side(cube_faces[a[0]], speed=speed)
             animations.append(rot1)
             animations.append(rot2)
         else:
             direction = -1 if "\'" in a else 1
-            rot = rotate_side(cube_faces[a[0]], direction=direction)
+            rot = rotate_side(cube_faces[a[0]], direction=direction, speed=speed)
             animations.append(rot)
 
     rubiks.show()
 
 
-
-
-
 def reset():
     scene.clear()
     init()
-def init():
+
+
+def init(state=None):
     global cubes, combine_parent, collider, rotation_helper
     combine_parent = Entity(enabled=False)
     for i in range(3):
@@ -246,16 +246,14 @@ def init():
                 e = Entity(model=copy(combine_parent.model), position=Vec3(x, y, z) - Vec3(3, 3, 3) / 3,
                            texture="white_cube")
                 cubes.append(e)
-
     rotation_helper = Entity()
     collider = Entity(model="cube", scale=3, collider="box", visible=False)
     collider.input = collider_input
 
-CONTROL_POS_GAP = 0.06
 
-shuffle_button = Button(text="Shuffle", color=color.azure, position=(.7, -.3), on_click=shuffle)
+shuffle_button = Button(text="Shuffle", color=color.azure, position=(.7, -.3), on_click=partial(shuffle, 0))
 shuffle_button.fit_to_text()
-solve_button = Button(text="Solve", color=color.azure, position=(.7, -.36), on_click=solve)
+solve_button = Button(text="Solve", color=color.azure, position=(.7, -.36), on_click=partial(solve, 1))
 solve_button.fit_to_text()
 
 init()
@@ -265,6 +263,6 @@ EditorCamera()
 if __name__ == '__main__':
     rubiks = RubiksCube(n=3)
 
-    window.size = (1200,600)
+    window.size = (1200, 600)
     window.update_aspect_ratio()
     app.run()
